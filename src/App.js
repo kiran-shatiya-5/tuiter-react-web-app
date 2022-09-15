5b960ab4cbc7a9edc0e2e046a353c9b87782b37f
Kiran  Shatiya <kiranshatiya@Kirans-MacBook-Pro.local>
2022-09-15 15:50:21 -0400

    Added changes to assignment 2 to test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> Welcome to Kiran's Web development Fall 2022 Assignment</h1>
+        <h2> This branch is with regards to Assignment 2</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
